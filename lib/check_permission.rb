3b785f1c3b1deef5d48edde58c67344935b5a9e3
sunil <sunil.kumar.14@netsolutionsindia.com>
2015-09-04 14:30:12 +0530

    dsdf

--- lib/check_permission.rb
+++ lib/check_permission.rb
@@ -3,33 +3,33 @@ require "check_permission/version"
 module CheckPermission
    def has_permission params
    		p "in check permission with #{params}"
-		# resource = params[:controller].singularize.camelize
-		# action = params[:action]
-		# begin
-		# 	permission = current_user.permissions			
-		# rescue
-		# 	return raise "current_user is nil"
-		# else
-		# 	return result permission, resource
-		# end		
+		resource = params[:controller].singularize.camelize
+		action = params[:action]
+		begin
+			permission = current_user.permissions			
+		rescue
+			return raise "current_user is nil"
+		else
+			return result permission, resource
+		end		
 	end
 
-	# private
+	private
 
-	# def result permission, resource		
-		# permission = current_user.permissions.where(resource_name: resource).last
-		# return false if permission.nil?
-		# case action.to_sym			
-		# when :index
-		# 	return permission.read_only		
-		# when :create
-		# 	return permission.create_only				
-		# when :update			
-		# 	return permission.update_only				 
-		# when :destroy, :delete			
-		# 	return permission.destroy_only		
-		# else
-		# 	return false
-		# end
-	# end
+	def result permission, resource		
+		permission = current_user.permissions.where(resource_name: resource).last
+		return false if permission.nil?
+		case action.to_sym			
+		when :index
+			return permission.read_only		
+		when :create
+			return permission.create_only				
+		when :update			
+			return permission.update_only				 
+		when :destroy, :delete			
+			return permission.destroy_only		
+		else
+			return false
+		end
+	end
 end
