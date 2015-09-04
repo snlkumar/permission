7f7b7acff25c59610381e5bc3edd77f28e31e58b
sunil <sunil.kumar.14@netsolutionsindia.com>
2015-09-04 18:22:54 +0530

    add helper

--- lib/generators/check_permission/check_permission_generator.rb
+++ lib/generators/check_permission/check_permission_generator.rb
@@ -2,15 +2,13 @@ require 'rails/generators/named_base'
 module CheckPermission
 	module Generators
 		class CheckPermissionGenerator < Rails::Generators::NamedBase
-			include Rails::Generators::ResourceHelpers
-			puts "pemission base name estart"
+			include Rails::Generators::ResourceHelpers			
 			namespace "check_permission"			
 			source_root File.expand_path("../templates", __FILE__)
 			desc "Generates a model with the given NAME (if one does not exist) with permission " <<
 			"configuration plus a migration file."
 			hook_for :orm
-			class_option :routes, desc: "Generate routes", type: :boolean, default: true	
-			puts "pemission base name end"		
+			class_option :routes, desc: "Generate routes", type: :boolean, default: true				
 		end
 	end
 end
